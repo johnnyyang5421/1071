0ceed6ee0b5214bac04c7912ebb0acd04b944184
johnnyyang5421 <43335323+johnnyyang5421@users.noreply.github.com>
2018-09-18 17:04:21 +0800

    Create Second change to H M S.cpp

---
+++ w02/Second change to H M S.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int S;
+	int hh,mm,ss;
+	printf("please Enter Second: ");
+	scanf("%d",&S);
+	hh=S/3600;
+	mm=S%3600/60;
+	ss=S%60;
+	 
+	printf("%d sec=%02d:%02d%:%02d\n",S,hh,mm,ss);
+}
